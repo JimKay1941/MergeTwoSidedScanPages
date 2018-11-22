22d31238e665d9eb6ca988dac6f4cf56ad4ad4c6
Jam Kay <jam@jimkay.us>
2018-11-22 18:14:36 +0800

    First working version

--- MergeTwoSidedScanPages/Form1.Designer.cs
+++ MergeTwoSidedScanPages/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.side1Path = new System.Windows.Forms.Label();
+            this.side2Path = new System.Windows.Forms.Label();
+            this.outputPath = new System.Windows.Forms.Label();
+            this.textSide1 = new System.Windows.Forms.TextBox();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.textSide2 = new System.Windows.Forms.TextBox();
+            this.textStatus = new System.Windows.Forms.TextBox();
+            this.btnInterleave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // side1Path
+            // 
+            this.side1Path.AutoSize = true;
+            this.side1Path.Location = new System.Drawing.Point(12, 25);
+            this.side1Path.Name = "side1Path";
+            this.side1Path.Size = new System.Drawing.Size(129, 13);
+            this.side1Path.TabIndex = 0;
+            this.side1Path.Text = "Choose Side-1 input path:";
+            this.side1Path.Click += new System.EventHandler(this.side1Path_Click);
+            // 
+            // side2Path
+            // 
+            this.side2Path.AutoSize = true;
+            this.side2Path.Location = new System.Drawing.Point(12, 73);
+            this.side2Path.Name = "side2Path";
+            this.side2Path.Size = new System.Drawing.Size(129, 13);
+            this.side2Path.TabIndex = 1;
+            this.side2Path.Text = "Choose Side-2 input path:";
+            this.side2Path.Click += new System.EventHandler(this.side2Path_Click);
+            // 
+            // outputPath
+            // 
+            this.outputPath.AutoSize = true;
+            this.outputPath.Location = new System.Drawing.Point(36, 121);
+            this.outputPath.Name = "outputPath";
+            this.outputPath.Size = new System.Drawing.Size(105, 13);
+            this.outputPath.TabIndex = 2;
+            this.outputPath.Text = "Choose Output path:";
+            this.outputPath.Click += new System.EventHandler(this.outputPath_Click);
+            // 
+            // textSide1
+            // 
+            this.textSide1.Location = new System.Drawing.Point(147, 22);
+            this.textSide1.Name = "textSide1";
+            this.textSide1.Size = new System.Drawing.Size(744, 20);
+            this.textSide1.TabIndex = 3;
+            this.textSide1.Text = "H:\\OneDrive\\documents\\Moving to US - Tucson\\Taiwan - Medical Records\\Taipei City " +
+    "Hospital - Both Branches\\Side-1s";
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(147, 118);
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(744, 20);
+            this.textOutput.TabIndex = 4;
+            this.textOutput.Text = "H:\\OneDrive\\documents\\Moving to US - Tucson\\Taiwan - Medical Records\\Taipei City " +
+    "Hospital - Both Branches";
+            // 
+            // textSide2
+            // 
+            this.textSide2.Location = new System.Drawing.Point(147, 70);
+            this.textSide2.Name = "textSide2";
+            this.textSide2.Size = new System.Drawing.Size(744, 20);
+            this.textSide2.TabIndex = 5;
+            this.textSide2.Text = "H:\\OneDrive\\documents\\Moving to US - Tucson\\Taiwan - Medical Records\\Taipei City " +
+    "Hospital - Both Branches\\Side-2s";
+            // 
+            // textStatus
+            // 
+            this.textStatus.Location = new System.Drawing.Point(147, 230);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.Size = new System.Drawing.Size(744, 20);
+            this.textStatus.TabIndex = 6;
+            // 
+            // btnInterleave
+            // 
+            this.btnInterleave.Location = new System.Drawing.Point(15, 160);
+            this.btnInterleave.Name = "btnInterleave";
+            this.btnInterleave.Size = new System.Drawing.Size(70, 38);
+            this.btnInterleave.TabIndex = 7;
+            this.btnInterleave.Text = "Interleave";
+            this.btnInterleave.UseVisualStyleBackColor = true;
+            this.btnInterleave.Click += new System.EventHandler(this.btnInterleave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(147, 160);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(70, 38);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(903, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnInterleave);
+            this.Controls.Add(this.textStatus);
+            this.Controls.Add(this.textSide2);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.textSide1);
+            this.Controls.Add(this.outputPath);
+            this.Controls.Add(this.side2Path);
+            this.Controls.Add(this.side1Path);
+            this.Name = "Form1";
+            this.Text = "Interleave Side1 and Side2 of Two-Sided document scanned in two parts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label side1Path;
+        private System.Windows.Forms.Label side2Path;
+        private System.Windows.Forms.Label outputPath;
+        private System.Windows.Forms.TextBox textSide1;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.TextBox textSide2;
+        private System.Windows.Forms.TextBox textStatus;
+        private System.Windows.Forms.Button btnInterleave;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MergeTwoSidedScanPages/Form1.cs
+++ MergeTwoSidedScanPages/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,155 @@ namespace MergeTwoSidedScanPages
 {
     public partial class Form1 : Form
     {
+        private readonly FolderBrowserDialog _chooseSide1FolderDialog = new FolderBrowserDialog();
+        private readonly FolderBrowserDialog _chooseSide2FolderDialog = new FolderBrowserDialog();
+        private readonly FolderBrowserDialog _chooseOutputFolderDialog = new FolderBrowserDialog();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void side1Path_Click(object sender, EventArgs e)
+        {
+            _chooseSide1FolderDialog.ShowDialog();
+            textSide1.Text = _chooseSide1FolderDialog.SelectedPath;
+        }
+
+        private void side2Path_Click(object sender, EventArgs e)
+        {
+            _chooseSide2FolderDialog.ShowDialog();
+            textSide2.Text = _chooseSide2FolderDialog.SelectedPath;
+        }
+
+        private void outputPath_Click(object sender, EventArgs e)
+        {
+            _chooseOutputFolderDialog.ShowDialog();
+            textOutput.Text = _chooseOutputFolderDialog.SelectedPath;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInterleave_Click(object sender, EventArgs e)
+        {
+            if (textSide1.Text == "")
+            {
+                textStatus.Text = @"Side1 Path is missing!";
+                return;
+            }
+
+            if (textSide2.Text == "")
+            {
+                textStatus.Text = @"Side2 Path is missing!";
+                return;
+            }
+
+            if (textOutput.Text == "")
+            {
+                textStatus.Text = @"Output Path is missing!";
+                return;
+            }
+
+            CopyMoveDirectory(new DirectoryInfo(textSide1.Text), (new DirectoryInfo(textSide2.Text)), textOutput.Text);
+        }
+
+        /// <summary>
+        ///     Recursive procedure to copy or move folder to another volume
+        /// </summary>
+        /// <param name="d">DirectoryInfo structure of origin</param>
+        /// <param name="destination">Destiny path, including folder</param>
+        private void CopyMoveDirectory(DirectoryInfo side1, DirectoryInfo side2, string destination)
+        {
+            var nameHold = "";
+
+            // Get files.
+            var filesS1 = side1.GetFiles();
+            var filesS2 = side2.GetFiles();
+
+            int fileNumberS1 = filesS1.Length;
+            int fileNumberS2 = filesS2.Length;
+
+            if (fileNumberS1 != fileNumberS2)
+            {
+                textStatus.Text = @"The Side1 and Side2 directories MUST have the same number of files! Aborting!";
+                return;
+            }
+
+            try
+            {
+                int inFileNumber = 0;
+                int outFileNumber = 1;
+                string strFileNumber1;
+                string strFileNumber2;
+                string outputFileName1 = "";
+                string outputFileName2 = "";
+
+                for (int i = 0; i < filesS1.Length; i++)
+                {
+                    strFileNumber1 = outFileNumber.ToString();
+                    strFileNumber2 = (outFileNumber + 1).ToString();
+
+                    if (strFileNumber1.Length == 1)
+                    {
+                        outputFileName1 = "Page00" + strFileNumber1.Substring(strFileNumber1.Length - 1, 1) + ".jpg";
+                    }
+
+                    else if (strFileNumber1.Length == 2)
+                    {
+                        outputFileName1 = "Page0" + strFileNumber1.Substring(strFileNumber1.Length - 2, 2) + ".jpg";
+                    }
+
+                    else if (strFileNumber1.Length == 3)
+                    {
+                        outputFileName1 = "Page" + strFileNumber1.Substring(strFileNumber1.Length - 3, 3) + ".jpg";
+                    }
+
+                    else
+                    {
+                        textStatus.Text = @"Output file numbering File1 has an overflow! " + strFileNumber1;
+                        return;
+                    }
+
+                    if (strFileNumber2.Length == 1)
+                    {
+                        outputFileName2 = "Page00" + strFileNumber2.Substring(strFileNumber2.Length - 1, 1) + ".jpg";
+                    }
+
+                    else if (strFileNumber2.Length == 2)
+                    {
+                        outputFileName2 = "Page0" + strFileNumber2.Substring(strFileNumber2.Length - 2, 2) + ".jpg";
+                    }
+
+                    else if (strFileNumber2.Length == 3)
+                    {
+                        outputFileName2 = "Page" + strFileNumber2.Substring(strFileNumber2.Length - 3, 3) + ".jpg";
+                    }
+
+                    else
+                    {
+                        textStatus.Text = @"Output file numbering File2 has an overflow! " + strFileNumber2;
+                        return;
+                    }
+
+                    // Just in case we make no changes
+                    var finalOutput1 = destination + "\\" + outputFileName1;
+                    var finalOutput2 = destination + "\\" + outputFileName2;
+
+                    filesS1[inFileNumber].CopyTo(finalOutput1);
+                    filesS2[inFileNumber].CopyTo(finalOutput2);
+
+                    inFileNumber += 1;
+                    outFileNumber += 2;
+                }
+            }
+            catch (PathTooLongException e)
+            {
+                textStatus.Text = e.Message;
+                return;
+            }
         }
     }
 }
